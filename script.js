c5ce44ada3851dde0622e2c32d39a68fe4b2148d
zafer genc <zafergenc5555@gmail.com>
2021-09-18 22:41:48 +0300

    form validation

---
+++ script.js
@@ -0,0 +1,66 @@
+const form = document.getElementById('form');
+const password1El = document.getElementById('password1');
+const password2El = document.getElementById('password2');
+const messageContainer = document.querySelector('.message-container');
+const message = document.getElementById('message');
+
+let isValid = false;
+let passworsMatch = false;
+
+function validateForm(){
+    // Using Constraint API
+    isValid = form.checkValidity();
+    // Style main message for an error
+    if(!isValid){
+        message.textContent = 'Please fill out all fields';
+        message.style.color = 'red';
+        messageContainer.style.borderColor = 'red';
+        return;
+    }
+
+    //  Check to see if passwords match
+    if(password1El.value === password2El.value){
+        passworsMatch = true;
+        password1El.style.borderColor = 'green';
+        password2El.style.borderColor = 'green';
+    }else{
+        passworsMatch = false;
+        message.textContent = 'Make sure passwords match!';
+        message.style.color = 'red';
+        messageContainer.style.borderColor = 'red';
+        password1El.style.borderColor = 'red';
+        password2El.style.borderColor = 'red';
+        return;
+    }
+
+    if(isValid && passworsMatch){
+        message.textContent = 'Successfully Registered!';
+        message.style.color = 'green';
+        messageContainer.style.borderColor = 'green';
+    }
+}
+
+function storeFormData(){
+    const user = {
+        name: form.name.value,
+        phone: form.phone.value,
+        email: form.email.value,
+        website: form.website.value,
+        password: form.password.value
+    };
+    //  No server yet :)
+    console.log(user);
+}
+
+function processFormData(e){
+    e.preventDefault();
+    
+    validateForm();
+
+    if(isValid && passworsMatch){
+        storeFormData();
+    }
+}
+
+// Event Listener
+form.addEventListener('submit', processFormData);
